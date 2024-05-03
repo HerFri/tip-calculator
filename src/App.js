1652efdbd5faa042c2a9edc72e3848426e177b21
Her Fri <herbert.frichert@outlook.de>
2024-05-03 21:21:38 +0200

    add static components

---
+++ src/App.js
@@ -0,0 +1,61 @@
+
+import './styles.css';
+
+export default function App() {
+  return (
+    <div>
+      <TipCalculator />
+    </div>
+  );
+}
+
+function TipCalculator() {
+  return (
+    <div>
+      <BillInput />
+      <SelectPercentage>How did you like the service?</SelectPercentage>
+      <SelectPercentage>How did your friend like the service?</SelectPercentage>
+      <Output />
+      <Reset />
+    </div>
+  );
+}
+
+
+function BillInput() {
+  return (
+    <div>
+      <label>How much was the bill?</label>
+      <input type="text" placeholder="Bill value" />
+    </div>
+  );
+}
+
+function SelectPercentage({children}) {
+  return (
+    <div>
+      <label>{children}</label>
+      <select>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolutely amazing! (20%)</option>
+
+      </select>
+    </div>
+  );
+}
+
+function Output() {
+  return (
+    <h3>You pay X ($A + $B tip)</h3>
+  );
+}
+
+function Reset() {
+  return (
+    <button>
+      Reset
+    </button>
+  );
+}
